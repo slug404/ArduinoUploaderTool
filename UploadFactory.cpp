b093a98f193b324eb59e1ae7639ebd5a752837ef
slug <slug404@hotmail.com>
2013-04-19 17:22:31 +0800

    完成了根据不同平台生成不同的Uploader的工厂类

--- UploadFactory.cpp
+++ UploadFactory.cpp
@@ -1,6 +1,32 @@
 #include "UploadFactory.h"
+#include "Uploader_Windows.h"
+#include "Uploader_Linux.h"
+#include "Uploader_Mac.h"
 
 UploadFactory::UploadFactory(QObject *parent)
     : QObject(parent)
 {
+}
+
+QSharedPointer<UploadBase> UploadFactory::create(UploadFactory::UploadPlatform platformType, const QString &codePath, const QString &serial, const QString &board)
+{
+	switch (platformType)
+	{
+		case OS_WINDOWS:
+		{
+			return QSharedPointer<UploadBase>(new Uploader_Windows(codePath, serial, board));
+		}
+		case OS_LINUX:
+		{
+			return QSharedPointer<UploadBase>(new Uploader_Linux(codePath, serial, board));
+		}
+		case OS_MAC:
+		{
+			return QSharedPointer<UploadBase>(new Uploader_Mac(codePath, serial, board));
+		}
+		default:
+		{
+			return QSharedPointer<UploadBase>(new Uploader_Windows(codePath, serial, board));//Windows用户最多
+		}
+	}
 }
--- UploadFactory.h
+++ UploadFactory.h
@@ -2,7 +2,8 @@
 #define UPLOADFACTORY_H
 
 #include <QObject>
-
+#include <QSharedPointer>
+#include "UploadBase.h"
 /*!
  * \brief 根据不同的运行平台(操作系统) 创建不同的处理对象的工厂
  */
@@ -12,6 +13,18 @@ class UploadFactory : public QObject
 public:
 	explicit UploadFactory(QObject *parent = 0);
 	
+	/**
+	 * @brief The UploadPlatform enum 运行平台
+	 */
+	enum UploadPlatform
+	{
+		OS_WINDOWS,/**< Windows平台*/
+		OS_LINUX,/**< Linux平台*/
+		OS_MAC/**< Mac平台*/
+	};
+
+	static QSharedPointer<UploadBase> create(UploadPlatform platformType, const QString &codePath, const QString &serial, const QString &board);
+
 signals:
 	
 public slots:
